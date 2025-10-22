9ec61d8e2c47550c018baa69c5b688f030ea047d
ambatucode <aprilianto665@gmail.com>
2025-10-22 22:47:40 +0700

    implement displayMenu method to show menu items

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,22 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        displayMenu();
+    }
+
+    public static void displayMenu() {
+        System.out.println("=== DAFTAR MENU ===");
+        
+        System.out.println("\nMAKANAN:");
+        System.out.println("- " + menuList[0].getName() + " : Rp " + menuList[0].getPrice());
+        System.out.println("- " + menuList[1].getName() + " : Rp " + menuList[1].getPrice());
+        System.out.println("- " + menuList[2].getName() + " : Rp " + menuList[2].getPrice());
+        System.out.println("- " + menuList[3].getName() + " : Rp " + menuList[3].getPrice());
+        
+        System.out.println("\nMINUMAN:");
+        System.out.println("- " + menuList[4].getName() + " : Rp " + menuList[4].getPrice());
+        System.out.println("- " + menuList[5].getName() + " : Rp " + menuList[5].getPrice());
+        System.out.println("- " + menuList[6].getName() + " : Rp " + menuList[6].getPrice());
+        System.out.println("- " + menuList[7].getName() + " : Rp " + menuList[7].getPrice());
     }
 }
