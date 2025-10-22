50b0a3651a6597bfea3de5d0c10ae9bdc371169d
ambatucode <aprilianto665@gmail.com>
2025-10-22 22:37:17 +0700

    add menuList initialization in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
+    private static final Menu[] menuList = {
+        new Menu("Nasi Padang", 25000, "makanan"),
+        new Menu("Ayam Bakar", 30000, "makanan"),
+        new Menu("Gado-gado", 20000, "makanan"),
+        new Menu("Soto Ayam", 22000, "makanan"),
+        new Menu("Es Teh", 8000, "minuman"),
+        new Menu("Jus Jeruk", 12000, "minuman"),
+        new Menu("Kopi", 10000, "minuman"),
+        new Menu("Air Mineral", 5000, "minuman")
+    };
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
